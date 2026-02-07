e344b7094734a5e31090b2533d2e056908fa177e
JustHarshit <hrpal00154s@gmail.com>
2026-02-07 22:50:02 +0530

    fixing npm run build

--- frontend/src/Components/Dashboard/RestaurantDashboard/Sidebar.js
+++ frontend/src/Components/Dashboard/RestaurantDashboard/Sidebar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <nav style={{ padding: '20px' }}>
-      <h2>FoodSathi Dashboard</h2>
+      <h2>SharePlate Dashboard</h2>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
         <li><Link to="/postDonation">Post Donation</Link></li>
         <li><Link to="/Leaderboard">Leaderboard</Link></li>
